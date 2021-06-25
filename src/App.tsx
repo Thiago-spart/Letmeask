c734f9cb2b127da8d2c167d97c9737309b2bd0ce
Thiago Moraes <user000.000resu@gmail.com>
2021-06-25 16:07:24 -0300

    Routing and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from "./pages/Home"
 import { NewRoom } from "./pages/NewRoom"
 
 export const App = () => {
   return (
-    <><NewRoom /></>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   )
 };
