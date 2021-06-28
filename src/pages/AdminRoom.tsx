82707a96a86df97ed849a03934e4583dd4ac80d6
Thiago Moraes <user000.000resu@gmail.com>
2021-06-28 02:32:20 -0300

    Create handleEndRoom function

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 import deleteImg from "../assets/delete.svg";
 import logoImg from "../assets/logo.svg";
@@ -17,13 +17,22 @@ type RoomParams = {
 
 export const AdminRoom = () => {
   // const { user } = useAuth();
+  const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const { title, questions } = useRoom(roomId);
 
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date(),
+    });
+
+    history.push("/");
+  }
+
   async function handleDeleteQuestion(questionId: string) {
     if (window.confirm("Tem certeza que você deseja excluir esta pergunta?")) {
-      await database.ref(`rooms/${roomId}/questions/${questions}`).remove();
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
   }
 
@@ -34,7 +43,9 @@ export const AdminRoom = () => {
           <img src={logoImg} alt="Letmeask" />
           <div>
             <RoomCode code={roomId} />
-            <Button isOutlined>Encerrar sala</Button>
+            <Button isOutlined onClick={handleEndRoom}>
+              Encerrar sala
+            </Button>
           </div>
         </div>
       </header>
